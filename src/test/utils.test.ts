d0a8f7c753bac26c34cc339839132af2b11040c2
Rubén Fernández Lopesino <ruben280994@gmail.com>
2023-03-21 12:13:00 +0100

    Multiple tests structure

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,26 +1,42 @@
-import { getStringInfo, toUpperCase } from "../app/utils";
+import { getStringInfo, StringInfo, toUpperCase } from "../app/utils";
+
+let sut = undefined; //sut it's from System Under Test
+let actual: StringInfo | undefined = undefined;
 
 describe("Utils test suite", () => {
+  beforeEach(() => {
+    sut = toUpperCase;
+  });
   it("Should return uppercase of valid string", () => {
     //Arrange
-    const sut = toUpperCase; //sut it's from System Under Test
     const expected = "TESTSTRING";
 
     //Act
-    const actual = sut("teststring");
+    actual = sut("teststring");
 
     //Assert
     expect(actual).toBe(expected);
   });
+});
 
-  it("Should return info of valid string", () => {
-    const sut = getStringInfo;
-
-    const actual = sut("My-String");
-
+describe("getStringInfo for arg My-String", () => {
+  beforeEach(() => {
+    sut = getStringInfo;
+    actual = sut("My-String");
+  });
+  it("Should return correct lowerCase", () => {
     expect(actual.lowerCase).toBe("my-string");
+  });
+  it("Should return correct upperCase", () => {
     expect(actual.upperCase).toBe("MY-STRING");
+  });
+
+  it("Should return correct characters length", () => {
     expect(actual.characters).toHaveLength(9);
+    expect(actual.length).toBe(9);
+  });
+
+  it("Should return correct characters", () => {
     expect(actual.characters).toEqual([
       "M",
       `y`,
@@ -32,7 +48,13 @@ describe("Utils test suite", () => {
       `n`,
       "g",
     ]);
-    expect(actual.length).toBe(9);
+  });
+
+  it("Should return correct extrainfo", () => {
     expect(actual.extraInfo).toEqual({});
+  });
+
+  it("Should return extrainfo defined", () => {
+    expect(actual.extraInfo).toBeDefined();
   });
 });
